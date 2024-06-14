4aec647ee28ba4bc7ced148f93033e0a6b5820e0
DankoKr <dankokralski776@gmail.com>
2024-06-14 14:47:14 +0200

    feat(Only admin can upload files)

--- API/routes/file.route.js
+++ API/routes/file.route.js
@@ -2,12 +2,19 @@ const express = require("express");
 const multer = require("multer");
 const { uploadFile, getFileById } = require("../controllers/file.controller");
 const { verifyToken } = require("../middlewares/auth.middleware");
+const { adminAuth } = require("../middlewares/adminAuth.middleware");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-// Route to upload file
-router.post("/upload", verifyToken, upload.single("file"), uploadFile);
+// Route to upload file only when user is ADMIN
+router.post(
+  "/upload",
+  verifyToken,
+  adminAuth,
+  upload.single("file"),
+  uploadFile
+);
 
 // Route to get file by id
 router.get("/files/:id", verifyToken, getFileById);
