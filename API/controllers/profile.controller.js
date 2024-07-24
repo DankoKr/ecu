503ffe1ee9b9f593e9f456c341e2c51dd88328ce
DankoKr <dankokralski776@gmail.com>
2024-07-24 12:42:30 +0200

    (feat) Get Endpoint for all users

--- API/controllers/profile.controller.js
+++ API/controllers/profile.controller.js
@@ -9,6 +9,20 @@ const fetchUserData = async (req, res) => {
   res.send(user);
 };
 
+const fetchAllUsers = async (req, res) => {
+  try {
+    const users = await db.User.findAll({
+      attributes: {
+        exclude: ["password", "image", "role", "username"], // Exclude sensitive fields
+      },
+    });
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error });
+  }
+};
+
 module.exports = {
   fetchUserData,
+  fetchAllUsers,
 };
--- API/routes/profile.route.js
+++ API/routes/profile.route.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { fetchUserData } = require("../controllers/profile.controller");
+const {
+  fetchUserData,
+  fetchAllUsers,
+} = require("../controllers/profile.controller");
 const { verifyToken } = require("../middlewares/auth.middleware");
+const { adminAuth } = require("../middlewares/adminAuth.middleware");
+
 const router = express.Router();
 
 // Route to fetch user profile info
 router.get("/profile", verifyToken, fetchUserData);
+
+// Route to fetch all users
+router.get("/users", verifyToken, adminAuth, fetchAllUsers);
 
 module.exports = router;
